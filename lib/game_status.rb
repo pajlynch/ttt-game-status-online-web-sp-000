a22a14127c2ae8940fb4f4f59de3c538fa2422bc
Patrick Lynch <pajlynch@gmail.com>
2021-03-18 23:55:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,3 +38,13 @@ def full?(board)
     return true
   end
 end
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif!full?(board) && !won?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
